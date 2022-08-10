58fd2da4c502b1150f9c0619003bc4f8e164d303
Art <iartrx@gmail.com>
2022-08-10 11:35:29 +0700

    completed playRound function

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ function getComputerChoice() {
     const computerPick = ["Rock", "Paper", "Scissors"];
     const randomChoice = computerPick[Math.floor(Math.random() * computerPick.length)]
 
-    console.log(randomChoice);
+    return randomChoice;
 }
 
-getComputerChoice();
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        console.log("You Win! " + playerSelection + " beats " + computerSelection + "!");
+    }
+    else if ((playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock") || (playerSelection === "Rock" && computerSelection === "Paper")) {
+        console.log("You Lose... " + computerSelection + " beats " + playerSelection + "!");
+    }
+    else {
+        console.log("Tie Game!");
+    }
+}
+
+
+playRound(playerSelection,computerSelection);
