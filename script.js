5b084c3132599252e90279dc507b4a3ecc14aea7
Art <iartrx@gmail.com>
2022-08-16 17:40:28 +0700

    added game function with game scorekeeper

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-function getComputerChoice() {
-    const computerPick = ["Rock", "Paper", "Scissors"];
+getComputerChoice = () => {
+    const computerPick = ["rock", "paper", "scissors"];
     const randomChoice = computerPick[Math.floor(Math.random() * computerPick.length)]
 
     return randomChoice;
 }
 
-const playerSelection = prompt("Rock, Paper or Scissors? Please Make Your Choice").toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
 
-console.log(playerSelection);
-console.log(computerSelection);
+playRound = (playerSelection, computerSelection) => {
+    if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+        return win;
+    }else if((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        return lose;
+    }else if(playerSelection === computerSelection) {
+        return tie;
+    }   
+}
 
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-        console.log("You Win! " + playerSelection + " beats " + computerSelection + "!");
+let win = `You won the round`;
+let lose = `You lost the round`;
+let tie = `Round tied`;
+
+
+game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+
+        let playerSelection = prompt("Choose your weapon: Rock, Paper or Scissors?").toLowerCase();
+        console.log(playerSelection);
+
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+
+        let result = (playRound(playerSelection, computerSelection));
+        if(result === win){
+            playerScore += 1;
+        }else if(result === lose){
+            computerScore += 1;
+        }
+
+        console.log(result);
     }
-    else if ((playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock") || (playerSelection === "rock" && computerSelection === "paper")) {
-        console.log("You Lose... " + computerSelection + " beats " + playerSelection + "!");
-    }
-    else if (playerSelection === computerSelection) {
-        console.log("Tie Game! You both played " + playerSelection);
+
+    if(playerScore > computerScore){
+        console.log(`Your Score is ${playerScore}. The Computer Score is ${computerScore}. You Won the Game!`)
+    }else if(computerScore > playerScore){
+        console.log(`Your Score is ${playerScore}. The Computer Score is ${computerScore}. You Lost the Game...`)
     }
 }
 
-playRound(playerSelection, computerSelection);
+
+game();
