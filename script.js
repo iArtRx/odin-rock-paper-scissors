f0b62c0912e73122dc0fc0c1aac7fc8f7dc2a46f
Art <iartrx@gmail.com>
2022-08-18 07:25:11 +0700

    toggle game over screen upon game end with message

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const scoreBoardMessage = document.querySelector("#scoreboardMessage");
 const displayPlayerScore = document.querySelector("#playerScore");
 const displayComputerScore = document.querySelector("#computerScore");
 const gameBoard = document.querySelector("#game-board");
+const displayGameOver = document.querySelector(".game-over");
 
 
 //variables
@@ -16,16 +17,14 @@ let computerScore = 0;
 //Game Logic
 getComputerChoice = () => {
     const computerPick = ["Rock", "Paper", "Scissors"];
-    const randomChoice = computerPick[Math.floor(Math.random() * computerPick.length)]
 
-    return randomChoice;
+    return random(computerPick);
 }
 
 playRound = (playerSelection) => {
     computerSelection = getComputerChoice();
-    console.log(computerSelection);
     if((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Rock")) {
-        scoreBoardMessage.textContent = `${playerSelection} beats ${computerSelection}`;
+        scoreBoardMessage.textContent = winMessage(playerSelection, computerSelection);
         playerScore ++;
     }else if((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {
         scoreBoardMessage.textContent = `${computerSelection} beats ${playerSelection}`;
@@ -60,11 +59,18 @@ buttons.forEach((button) => {
 });
 // Game Over Screen after the player or computer reaches the maximum score. 
 gameOver = () => {
-    console.log("Game Over");
     gameBoard.style.display = "none";
-    playerScore = 0;
-    computerScore = 0;
-    start.style.display = "block";
+    scoreBoard.style.display = "none";
+    displayGameOver.style.display = "block";
+    const para = document.createElement("p");
+    
+    if(playerScore === 5){
+        para.textContent = "Congratulations! You are Victorious!";
+        displayGameOver.append(para);
+    }else {
+        para.textContent = "You have been defeated...";
+        displayGameOver.append(para);
+    }
 }
 
 // Scoreboard that keeps track of the score.
@@ -72,6 +78,20 @@ scoreboard = () => {
     displayPlayerScore.textContent = `Player: ${playerScore}`;
     displayComputerScore.textContent = `Computer: ${computerScore}`;
 };
+
+winMessage = (playerSelection, computerSelection) => {
+    const winArray = [];
+    winArray[0] = `You win this round! ${playerSelection} beats ${computerSelection}`;
+    winArray[1] = `${computerSelection} was no match for ${playerSelection}. You win!`;
+    winArray[2] = `${playerSelection} dominates ${computerSelection}. You take this round!`;
+    return random(winArray);
+}
+
+//Helper Functions
+random = (array) => {
+    return array[[Math.floor(Math.random() * array.length)]];
+} 
+
 
 
 
